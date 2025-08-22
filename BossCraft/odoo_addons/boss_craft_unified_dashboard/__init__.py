7d0926b687a3edc01c5eff93a4e1f79b725e033e
Mingming <98148452+chao121026@users.noreply.github.com>
2025-08-22 23:37:57 +0800

    Delete BossCraft directory

--- BossCraft/odoo_addons/boss_craft_unified_dashboard/__init__.py
+++
@@ -1,3 +0,0 @@
-# Import the models and controllers so Odoo can discover them
-from . import models
-from . import controllers
--- BossCraft/odoo_addons/boss_craft_unified_dashboard/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# Import your model files
-from . import unified_dashboard
-from . import sale_order
