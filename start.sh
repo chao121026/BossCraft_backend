35650bc7052119c4bbc4800df13646dd7cdf9bae
Mingming <98148452+chao121026@users.noreply.github.com>
2025-08-23 00:19:42 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# 使用 envsubst 命令来替换模板文件中的环境变量
-# 然后将输出写入到 Odoo 的实际配置文件中
+# 使用 envsubst 替换环境变量
 envsubst < /etc/odoo/odoo.conf.template > /etc/odoo/odoo.conf
 
-# 执行原始的 Docker CMD，启动 Odoo
+# 启动 Odoo
 exec "$@"
